2cdf2fcfffafb0d9676aee8619c6ef1419cfd2e7
Alexander-2212 <kaminskiyale@gmail.com>
2023-08-27 23:24:21 +0300

    Solution class added, Program class edited

--- Project_11/Program.cs
+++ Project_11/Program.cs
@@ -10,6 +10,31 @@ namespace Project_11
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.WriteLine("Введіть коефіцієнти a, b і c для квадратного рівняння ax^2 + bx + c = 0:");
+
+            double a, b, c;
+
+            Console.Write("a: ");
+            while (!double.TryParse(Console.ReadLine(), out a))
+                Console.WriteLine("Ви ввели некоректне значення коефіцієнту, спробуйте знову");
+
+            Console.Write("b: ");
+            while (!double.TryParse(Console.ReadLine(), out b))
+                Console.WriteLine("Ви ввели некоректне значення коефіцієнту, спробуйте знову");
+
+            Console.Write("c: ");
+            while (!double.TryParse(Console.ReadLine(), out c))
+                Console.WriteLine("Ви ввели некоректне значення коефіцієнту, спробуйте знову");
+
+            Task task = new Task(a, b, c);
+            Solver solver = new Solver();
+            Solution solution = solver.Solve(task);
+
+            Console.WriteLine(solution);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Project_11/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_11
+{
+    internal class Solution
+    {
+        public double? X1 { get; }
+        public double? X2 { get; }
+        public string Status { get; }
+
+        public Solution(double x1, double x2)
+        {
+            X1 = x1;
+            X2 = x2;
+            Status = "Два корені";
+        }
+
+        public Solution(double x)
+        {
+            X1 = x;
+            Status = "Один корінь";
+        }
+
+        public Solution()
+        {
+            Status = "Немає коренів";
+        }
+
+        public override string ToString()
+        {
+            switch (Status)
+            {
+                case "Два корені":
+                    return $"Корені: x1 = {X1}, x2 = {X2}";
+                case "Один корінь":
+                    return $"Корінь: x = {X1}";
+                default:
+                    return "Немає коренів";
+            }
+        }
+    }
+}
