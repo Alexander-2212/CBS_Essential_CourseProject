647b6604652fb16b0611b5da3f91313cb3720458
Alexander-2212 <kaminskiyale@gmail.com>
2023-08-27 17:52:29 +0300

    Added Task class

---
+++ Project_11/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_11
+{
+    internal class Task
+    {
+        public double A { get; }
+        public double B { get; }
+        public double C { get; }
+
+        public Task(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+    }
+}
