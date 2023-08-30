47a951e3f6f38f0bbd46cdbd1be2587c5c6e44db
Alexander-2212 <kaminskiyale@gmail.com>
2023-08-30 15:16:15 +0300

    Project 13 Added

---
+++ Project_13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Point point1 = new Point(0, 0);
+            Point point2 = new Point(6, 4);
+
+            Rectangle rectangle = new Rectangle(point1, point2);
+
+            Console.WriteLine($"Площа прямокутника: {rectangle.Area()}");
+            Console.WriteLine($"Периметр прямокутника: {rectangle.Perimeter()}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Project_13/Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_13
+{
+    internal class Rectangle
+    {
+        public Point Point1 { get; set; }
+        public Point Point2 { get; set; }
+
+        public Rectangle(Point point1, Point point2)
+        {
+            if (point1.X == point2.X || point1.Y == point2.Y)
+                throw new ArgumentException("Дві точки лежать на одній прямій!");
+
+            Point1 = point1;
+            Point2 = point2;
+        }
+
+        public double Area()
+        {
+            return Math.Abs(Point1.X - Point2.X) * Math.Abs(Point1.Y - Point2.Y);
+        }
+
+        public double Perimeter()
+        {
+            return 2 * (Math.Abs(Point1.X - Point2.X) + Math.Abs(Point1.Y - Point2.Y));
+        }
+
+    }
+}
